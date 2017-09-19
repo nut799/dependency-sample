e2f05b2c68aa0f6d126f2709eb1ea449694ab37a
Paulo Coutinho <paulo@prsolucoes.com>
2017-09-19 20:26:50 -0300

    initial version

--- build/bridge/HelloWorldBridgeImpl.cpp
+++ build/bridge/HelloWorldBridgeImpl.cpp
@@ -6,18 +6,19 @@ namespace Ezored {
 		std::shared_ptr<HelloWorld> helloWorld = std::make_shared<HelloWorld>();
 		std::shared_ptr<HelloWorldBridgeImpl> bridge = std::make_shared<HelloWorldBridgeImpl>();
 		*bridge->setHelloWorld(helloWorld);
+		return bridge;
     }
 
     void HelloWorldBridgeImpl::set_greeting_message(const std::string &message) {
-        // this->message = message;
+        *helloWorld->setGreetingMessage(message);
     }
 
     std::string HelloWorldBridgeImpl::get_greeting_message() {
-        return "Hello world";
+		return *helloWorld->getGreetingMessage();
     }
 
     void HelloWorldBridgeImpl::show_greeting_message() {
-        std::cout << "Message: " << "Hello world" << std::endl;
+		*helloWorld->showGreetingMessage();		
     }
 
 }
