1c2887aaf2edefa31ae9d3becc7c4a65728bb1c3
Paulo Coutinho <paulo@prsolucoes.com>
2017-12-01 17:27:58 -0200

    initial commit

--- build.py
+++ build.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 
-dependency_name = 'ezored-sample'
+dependency_name = 'dependency-sample'
 
-root_dir = os.getenv(key='EZORED_PROJECT_ROOT')
+home_dir = os.getenv(key='EZORED_PROJECT_HOME')
 current_dir = os.getcwd()
-vendor_dir = os.path.join(root_dir, 'vendor')
-temp_dir = os.path.join(root_dir, 'temp')
+vendor_dir = os.path.join(home_dir, 'vendor')
+temp_dir = os.path.join(home_dir, 'temp')
 
 print "Sample :: Build script"
-print "Sample :: EZORED_PROJECT_ROOT: %s" % (root_dir)
+print "Sample :: EZORED_PROJECT_HOME: %s" % (home_dir)
 print "Sample :: WORKING_DIR: %s" % (current_dir)
 print "Sample :: VENDOR_DIR: %s" % (vendor_dir)
 print "Sample :: TEMP_DIR: %s" % (temp_dir)
