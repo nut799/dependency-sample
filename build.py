56a07584eae1e5f6f73983d90c44dafad9b6ea90
Paulo Coutinho <paulo@prsolucoes.com>
2017-12-02 03:43:55 -0200

    initial commit

--- build.py
+++ build.py
@@ -5,18 +5,18 @@ dependency_name = 'dependency-sample'
 
 home_dir = os.getenv(key='EZORED_PROJECT_HOME')
 current_dir = os.getcwd()
-vendor_dir = os.path.join(home_dir, 'vendor')
-temp_dir = os.path.join(home_dir, 'temp')
+vendor_dir = os.getenv(key='EZORED_REPOSITORY_VENDOR_DIR')
+temp_dir = os.getenv(key='EZORED_REPOSITORY_TEMP_DIR')
 
-print "Sample :: Build script"
-print "Sample :: EZORED_PROJECT_HOME: %s" % (home_dir)
-print "Sample :: WORKING_DIR: %s" % (current_dir)
-print "Sample :: VENDOR_DIR: %s" % (vendor_dir)
-print "Sample :: TEMP_DIR: %s" % (temp_dir)
+print('Sample :: Build script')
+print('Sample :: EZORED_PROJECT_HOME: {0}'.format(home_dir))
+print('Sample :: WORKING_DIR: {0}'.format(current_dir))
+print('Sample :: VENDOR_DIR: {0}'.format(vendor_dir))
+print('Sample :: TEMP_DIR: {0}'.format(temp_dir))
 
 try:
     shutil.rmtree(path=os.path.join(vendor_dir, dependency_name))
 except:
     pass
 
-shutil.copytree(os.path.join(current_dir, 'build'), os.path.join(vendor_dir, dependency_name))
+shutil.copytree(os.path.join(current_dir, 'build'), vendor_dir)
